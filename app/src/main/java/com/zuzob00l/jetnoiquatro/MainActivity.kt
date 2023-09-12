dd34ffb296542235429c5f21915442ed09b5713e
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-09-12 21:06:09 +0200

    call start screen from Main

--- app/src/main/java/com/zuzob00l/jetnoiquatro/MainActivity.kt
+++ app/src/main/java/com/zuzob00l/jetnoiquatro/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.zuzob00l.jetnoiquatro.ui.screens.StartScreen
 import com.zuzob00l.jetnoiquatro.ui.theme.JetNoiQuatroTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    StartScreen(onStartClick ={})
                 }
             }
         }
