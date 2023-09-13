a64cb80ec1fb050b2efdee147690453c8c18e683
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-09-13 18:49:13 +0200

    add animations and icons dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,9 @@ dependencies {
     //coroutine
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 
+    //compose icons
+    implementation("androidx.compose.material:material-icons-extended:1.2.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
