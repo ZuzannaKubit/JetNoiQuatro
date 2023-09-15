73b54f5f495c487ad74f7bffad23842fab23f887
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-09-15 14:53:49 +0200

    Create UserData class
    class, witch describe user

---
+++ app/src/main/java/com/zuzob00l/jetnoiquatro/data/UserData.kt
@@ -0,0 +1,10 @@
+package com.zuzob00l.jetnoiquatro.data
+
+data class UserData(
+    val id: Long,
+    val name: String,
+    val surname: String,
+    val address: String,
+    val email: String,
+    val password: String
+)
